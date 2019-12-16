a43e2745026cc098865016d0022b8e6d171acb41
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-17 00:32:49 +0530

    (feature) Returns output for a permitted commands sent from telegram

--- bin/properties.py
+++ bin/properties.py
@@ -1,3 +1,4 @@
 token_val = "852000759:AAHPVSGqC_Z7NQI_q9NhS3FF-bfyZT-wcKc"
-logging_location = "../logs/"
+logging_location = "../logs/bot.log"
 media_location = "../media/"
+permitted_commands =['ls']
