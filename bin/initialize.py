53eab0915746745bd806a881eecdf247dfc63f78
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-17 01:09:14 +0530

    (refactor) Changed naming of files to be more sensible

---
+++ bin/initialize.py
@@ -0,0 +1,8 @@
+from properties import *
+import subprocess
+
+logging_location = log_location + log_name
+subprocess.call(['mkdir','-p',log_location])
+subprocess.call(['touch',logging_location])
+subprocess.call(['pip','install','-r','../requirements.txt'])
+subprocess.call(['python','start.py'])
