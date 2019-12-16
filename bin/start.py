36467d1dc2a246d89772c2b23eeaea486f6dac59
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-17 01:00:24 +0530

    (feature) Added a script to intialize different folders and requirements

---
+++ bin/start.py
@@ -0,0 +1,7 @@
+from properties import *
+import subprocess
+
+logging_location = log_location + log_name
+subprocess.call(['mkdir','-p',log_location])
+subprocess.call(['touch',logging_location])
+subprocess.call(['pip','install','-r','../requirements.txt'])
